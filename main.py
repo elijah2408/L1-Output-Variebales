7766e3d87edde2d67f4bde743c09a444163560d4
elijah24 <107879320+elijah2408@users.noreply.github.com>
2022-06-20 18:16:35 +0000

    concatenation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("Hello World") # This prints out Hello World
 print("My name is Elijah")
 print("Hello " + "World")
+
+myNAME = "Elijah" # String
+myBirthMonth = 1 # Integer
+myBirthDate = "28" #String
+pi = 3.14 # float
+isHungry = True # Boolean 
+
+print("My name is" + myNAME)
+print("My birth month is " + str(myBirthMonth))
+
